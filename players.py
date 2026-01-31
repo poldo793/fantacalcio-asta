19929ffe33c9bdafd44403d6ce617ec67a4c6713
poldo793 <simonepolo.sp@gmail.com>
2026-01-31 17:13:36 +0100

    players.py

--- players.py
+++ players.py
@@ -1,1 +1,11 @@
-players = []
+# players.py
+# Il 2 febbraio sostituisci questa lista con quella finale (uno per riga).
+PLAYERS = [
+    "Parisi",
+    "Raspadori",
+    "Lukaku",
+    "Romagnoli",
+    "McKennie",
+]
+
+
